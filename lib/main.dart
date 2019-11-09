3ffeb47501dde8e4154d81fa978c6d03df7e187d
Aakash Padhiyar <aakashpadhiyar06@gmail.com>
2019-11-09 10:51:25 +0530

    dicee app

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Center(
+            child: Text('Dicee'),
+          ),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Image(
+          width: 200.0,
+          image: AssetImage('images/dice1.png'),
+        )
+      ],
+    );
+  }
+}
